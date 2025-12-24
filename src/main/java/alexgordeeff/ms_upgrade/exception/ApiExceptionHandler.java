03ddbacaeb1928acd16aede4ce86074af296192e
jazzboy65 <alexis.gordeeff@gmail.com>
2025-12-24 22:40:50 +0500

    UPGAG-7: change ex handler

--- src/main/java/alexgordeeff/ms_upgrade/exception/ApiExceptionHandler.java
+++ src/main/java/alexgordeeff/ms_upgrade/exception/ApiExceptionHandler.java
@@ -8,8 +8,8 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 public class ApiExceptionHandler {
 
-    @ExceptionHandler(ApiException.class)
-    public ResponseEntity<ErrorResponse> handleApiException(ApiException ex ) {
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<ErrorResponse> handleApiException(ApiException ex) {
         var error = new ErrorResponse(
                 ex.getErrorCode(), ex.getMessage(), ex.getHttpStatus().value());
         return new ResponseEntity<>(error, ex.getHttpStatus());
